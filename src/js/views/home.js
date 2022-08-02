2ae9e556e2b59f8d47c898fb036a78beaf99d568
Mario <waro.brambilla@gmail.com>
2022-08-02 14:49:25 +0000

    update

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,72 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import { Scrollbars } from 'react-custom-scrollbars-2';
+
+
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div>
+	<Scrollbars style={{ width: 500, height: 300 }}> 
+		<CardGroup> 
+			<Card className="m-3">
+				<Card.Img variant="top" src="https://picsum.photos/400/200" />
+				<Card.Body>
+					<Card.Title>Card Title</Card.Title>
+					<Card.Text>
+					Some quick example text to build on the card title and make up the
+					bulk of the card's content.
+					</Card.Text>
+					<Button variant="primary">Go somewhere</Button>
+				</Card.Body>
+			</Card>
+			<Card className="m-3">
+				<Card.Img variant="top" src="https://picsum.photos/400/200" />
+				<Card.Body>
+					<Card.Title>Card Title</Card.Title>
+					<Card.Text>
+					Some quick example text to build on the card title and make up the
+					bulk of the card's content.
+					</Card.Text>
+					<Button variant="primary">Go somewhere</Button>
+				</Card.Body>
+			</Card>
+			<Card className="m-3">
+				<Card.Img variant="top" src="https://picsum.photos/400/200" />
+				<Card.Body>
+					<Card.Title>Card Title</Card.Title>
+					<Card.Text>
+					Some quick example text to build on the card title and make up the
+					bulk of the card's content.
+					</Card.Text>
+					<Button variant="primary">Go somewhere</Button>
+				</Card.Body>
+			</Card>
+			<Card className="m-3">
+				<Card.Img variant="top" src="https://picsum.photos/400/200" />
+				<Card.Body>
+					<Card.Title>Card Title</Card.Title>
+					<Card.Text>
+					Some quick example text to build on the card title and make up the
+					bulk of the card's content.
+					</Card.Text>
+					<Button variant="primary">Go somewhere</Button>
+				</Card.Body>
+			</Card>
+			<Card className="m-3">
+				<Card.Img variant="top" src="https://picsum.photos/400/200" />
+				<Card.Body>
+					<Card.Title>Card Title</Card.Title>
+					<Card.Text>
+					Some quick example text to build on the card title and make up the
+					bulk of the card's content.
+					</Card.Text>
+					<Button variant="primary">Go somewhere</Button>
+				</Card.Body>
+			</Card>
+		</CardGroup>
+	</Scrollbars>
 	</div>
-);
+)
